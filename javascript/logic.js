473ba470c41786221a350135f88ae0e32a2b46d1
Lucky Penny <41957862+smanhart@users.noreply.github.com>
2018-11-09 14:42:42 -0700

    added header w/title, tweaked button colors, card colors

--- javascript/logic.js
+++ javascript/logic.js
@@ -24,16 +24,16 @@ console.log(title);
      console.log(recipe);
 
 
-  $.ajax({
-    method: "POST",
-    url: "https://api.edamam.com/api/nutrition-details?app_id=ce13381c&app_key=4967284ccc86e52d874362eb512b2e94",
-    dataType: 'json',
-    contentType: 'application/json',
-    data: JSON.stringify(recipe)
-  }).then(function(response) {
-    //console.log(response);
-    callback(response);
-  })
+//   $.ajax({
+//     method: "POST",
+//     url: "https://api.edamam.com/api/nutrition-details?app_id=ce13381c&app_key=4967284ccc86e52d874362eb512b2e94",
+//     dataType: 'json',
+//     contentType: 'application/json',
+//     data: JSON.stringify(recipe)
+//   }).then(function(response) {
+//     //console.log(response);
+//     callback(response);
+//   })
 
 };
 
@@ -69,7 +69,7 @@ function recipeSearch(ingredient) {
     //takes the commas out of the array before adding to url
     queryParams = params.join("")
 
-    queryURL = "https://api.yummly.com/v1/api/recipes?_app_id=1bdad67c&_app_key=d635ffbe690df5a2a7005bdce55a1164&q=" + ingredient + queryParams + "&maxResult=3&requirePictures=true"
+    queryURL = "https://api.yummly.com/v1/api/recipes?_app_id=1bdad67c&_app_key=d635ffbe690df5a2a7005bdce55a1164&q=" + ingredient + queryParams + "&maxResult=4&requirePictures=true"
 
     // console.log(queryURL)
 
@@ -78,12 +78,13 @@ function recipeSearch(ingredient) {
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response)
+        console.log("length of response matches" + response.matches.length)
 
         for (var i = 0; i < response.matches.length; i++) {
-
+            console.log("current for loop index" + i)
             ingrId = response.matches[i].id;
             grabFullRecipe()
+
         }
     })
 }
